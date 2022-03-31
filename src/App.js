90a64176562ea11218bef4481d952ca8b6e70700
Maximilian Thiel <maximilianthiel@maximilians-mbp.myfiosgateway.com>
2022-03-31 16:32:18 -0400

    setting up skeleton for form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css';
 
+const initialFormValues = {
+  firstName: '',
+  lastName: '',
+  email: '',
+  role: '',
+}
+
+
+
 function App() {
+  const [teamMembers, setTeamMembers] = useState([]);
+
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formError, setFormError] = useState("");
+
+  const updateForm = (inputName, inputValue )
+
+  useEffect(() => {
+   
+  })
+
   return (
     <div className="App">
       <header className="App-header">
---
+++ src/Form.js
@@ -0,0 +1,50 @@
+import React from "react"
+
+  export default function Form(props) {
+      const { values, update, submit } = props
+
+
+
+    return (
+        <form>
+            <div>
+                <label> First Name
+                    <input
+                    type="text"
+                    name="firstName"
+                    value={values.firstName}
+                    onChange={onChange}
+                    />
+                </label>
+                <label> Last Name
+                    <input
+                    type="text"
+                    name="lastName"
+                    value={values.lastName}
+                    onChange={onChange}
+                    />
+                </label>
+                <label> Email
+                    <input
+                    type="email"
+                    name="email"
+                    value={values.email}
+                    onChange={onChange}
+                    />
+                </label>
+                <label> Role
+                    <select value={values.role} name="role" onChange={onChange}>
+                        <option value="">-- Select a Role --</option>
+                        <option value="Student">frontend engineer</option>
+                        <option value="Instructor">backend engineer</option>
+                        <option value="Alumni">desinger</option>
+                        <option value="Alumni">wizard</option>
+                    </select>
+                </label>
+            </div>
+        </form>
+    )
+
+
+
+  }
